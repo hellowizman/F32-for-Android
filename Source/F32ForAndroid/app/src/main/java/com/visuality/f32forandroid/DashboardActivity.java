e1e5b7a41f8aa7da09ad70016ff384ac06366d93
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-04-15 23:37:53 +0300

    Updated demo project

--- Source/F32ForAndroid/app/src/main/java/com/visuality/f32forandroid/DashboardActivity.java
+++ Source/F32ForAndroid/app/src/main/java/com/visuality/f32forandroid/DashboardActivity.java
@@ -3,11 +3,34 @@ package com.visuality.f32forandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.visuality.f32.weather.data.entity.Weather;
+import com.visuality.f32.weather.manager.WeatherManager;
+
 public class DashboardActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "5bc694edded18d31b165727e6fb6be8f";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        /**
+         * Test weather manager.
+         */
+
+        new WeatherManager(API_KEY).getCurrentWeather(
+                47.2257,
+                38.9383,
+                new WeatherManager.CurrentWeatherHandler() {
+                    @Override
+                    public void onReceivedCurrentWeather(WeatherManager manager, Weather weather) {
+                    }
+
+                    @Override
+                    public void onFailedToReceiveCurrentWeather(WeatherManager manager) {
+                    }
+                }
+        );
     }
 }
