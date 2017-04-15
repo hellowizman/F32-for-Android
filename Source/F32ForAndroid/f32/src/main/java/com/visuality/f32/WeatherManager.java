288af3577b72da45dfec26ff86f63b7e6cfc265b
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-04-15 15:32:49 +0300

    Added project

---
+++ Source/F32ForAndroid/f32/src/main/java/com/visuality/f32/WeatherManager.java
@@ -0,0 +1,12 @@
+package com.visuality.f32;
+
+/**
+ * Created by igormatyushkin on 15.04.17.
+ */
+
+public class WeatherManager {
+
+    public WeatherManager(String apiKey) {
+        super();
+    }
+}
