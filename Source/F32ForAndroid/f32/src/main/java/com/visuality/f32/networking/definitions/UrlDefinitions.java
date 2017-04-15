6eb746defc3506a1849afc85cca8427779ee28cd
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-04-15 17:52:46 +0300

    Added URL definitions

---
+++ Source/F32ForAndroid/f32/src/main/java/com/visuality/f32/networking/definitions/UrlDefinitions.java
@@ -0,0 +1,10 @@
+package com.visuality.f32.networking.definitions;
+
+/**
+ * Created by igormatyushkin on 15.04.17.
+ */
+
+public class UrlDefinitions {
+
+    public static String BASE_URL = "http://openweathermap.org/data/2.5";
+}
