31735911e20d605378f5b5989051c2e74705070d
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-04-25 23:11:11 +0300

    Added relative path to forecast

--- Source/F32ForAndroid/f32/src/main/java/com/visuality/f32/networking/definitions/UrlDefinitions.java
+++ Source/F32ForAndroid/f32/src/main/java/com/visuality/f32/networking/definitions/UrlDefinitions.java
@@ -9,4 +9,6 @@ public final class UrlDefinitions {
     public static String BASE_URL = "http://api.openweathermap.org/data/2.5";
 
     public static String RELATIVE_PATH_TO_CURRENT_WEATHER = "weather";
+
+    public static String RELATIVE_PATH_TO_FORECAST = "forecast";
 }
