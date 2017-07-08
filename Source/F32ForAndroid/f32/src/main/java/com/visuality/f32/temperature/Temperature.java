87af0028c998d2f0b466e65b7148e91a8778ad57
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-07-09 01:41:50 +0300

    Changes for temperature conversion algorithm

--- Source/F32ForAndroid/f32/src/main/java/com/visuality/f32/temperature/Temperature.java
+++ Source/F32ForAndroid/f32/src/main/java/com/visuality/f32/temperature/Temperature.java
@@ -41,13 +41,13 @@ public class Temperature {
                     case CELCIUS:
                         return this.value;
                     case DELISLE:
-                        return (this.value * 1.5) - 100.0;
+                        return (100.0 - this.value) * 1.5;
                     case FAHRENHEIT:
                         return (this.value * 1.8) + 32.0;
                     case KELVIN:
                         return this.value + 273.15;
                     case RANKINE:
-                        return (this.value * 1.8) + 32.0 + 459.67;
+                        return (this.value * 1.8) + 491.67;
                     case REAUMUR:
                         return this.value * 0.8;
                     case ROMER:
@@ -58,19 +58,19 @@ public class Temperature {
             case DELISLE:
                 switch (targetUnit) {
                     case CELCIUS:
-                        return (this.value + 100.0) / 1.5;
+                        return (100.0 - this.value) * (2.0 / 3.0);
                     case DELISLE:
                         return this.value;
                     case FAHRENHEIT:
-                        return (this.value + 100.0) * 1.2 + 32.0;
+                        return 212.0 - (this.value * 1.2);
                     case KELVIN:
-                        return ((this.value + 100.0) / 1.5) + 273.15;
+                        return 373.15 - (this.value * 2.0 / 3.0);
                     case RANKINE:
-                        return (this.value + 100.0) * 1.2 + 491.67;
+                        return 671.67 - (this.value * 1.2);
                     case REAUMUR:
-                        return (this.value + 100.0) * 0.53333;
+                        return 80.0 - (this.value * 8.0 / 15.0);
                     case ROMER:
-                        return (this.value + 100.0) * 0.35 + 7.5;
+                        return 60.0 - (this.value * 0.35);
                     default:
                         return this.value;
                 }
@@ -89,7 +89,7 @@ public class Temperature {
                     case REAUMUR:
                         return (this.value - 32.0) / 2.25;
                     case ROMER:
-                        return ((this.value - 32.0) * (7.0 / 24.0)) + 7.5;
+                        return (this.value - 32.0) * (7.0 / 24.0) + 7.5;
                     default:
                         return this.value;
                 }
@@ -108,14 +108,14 @@ public class Temperature {
                     case REAUMUR:
                         return (this.value - 273.15) * 0.8;
                     case ROMER:
-                        return ((this.value - 273.15) * (21.0 / 40.0)) + 7.5;
+                        return (this.value - 273.15) * (21.0 / 40.0) + 7.5;
                     default:
                         return this.value;
                 }
             case RANKINE:
                 switch (targetUnit) {
                     case CELCIUS:
-                        return (this.value - 32.0 - 459.67) / 1.8;
+                        return (this.value / 1.8) - 273.15;
                     case DELISLE:
                         return (671.67 - this.value) * (5.0 / 6.0);
                     case FAHRENHEIT:
@@ -125,24 +125,24 @@ public class Temperature {
                     case RANKINE:
                         return this.value;
                     case REAUMUR:
-                        return (this.value - 32.0 - 459.67) / 2.25;
+                        return ((this.value / 1.8) + 273.15) * 0.8;
                     case ROMER:
-                        return ((this.value - 491.67) * (7.0 / 24.0)) + 7.5;
+                        return (this.value - 491.67) * (7.0 / 24.0) + 7.5;
                     default:
                         return this.value;
                 }
             case REAUMUR:
                 switch (targetUnit) {
                     case CELCIUS:
-                        return this.value * 1.25;
+                        return this.value / 0.8;
                     case DELISLE:
-                        return (80.0 - this.value) * (15.0 / 8.0);
+                        return (80.0 - this.value) * 1.875;
                     case FAHRENHEIT:
                         return (this.value * 2.25) + 32.0;
                     case KELVIN:
-                        return (this.value * 1.25) + 273.15;
+                        return (this.value / 0.8) + 273.15;
                     case RANKINE:
-                        return (this.value * 2.25) + 32.0 + 459.67;
+                        return (this.value * 2.25) + 491.67;
                     case REAUMUR:
                         return this.value;
                     case ROMER:
